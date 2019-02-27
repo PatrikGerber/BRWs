f16bf77bb1883daeeb78c5b055855e7a05c37f74
PatrikGerber <patrikgerber96@gmail.com>
2019-02-27 20:16:48 +0000

    Started working on polynomial tails and adding mathematical background

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 import scipy.stats as stat
 import numpy as np
+import matplotlib.pyplot as plt
 
 class pointProcess:
 	def __init__(self):
@@ -7,20 +8,22 @@ class pointProcess:
 
 	def simulate(self):
 		n = 2
-		return stat.norm.rvs(size=n)
+		return stat.rayleigh.rvs(scale = 4, size=n)
 
 class N_BRW:
 	def __init__(self, pointProcess, N, startPos = None):
 		self.N = N
 		self.pointProcess = pointProcess
-		self.startPos = startPos if startPos else [0]*N
+		self.currPos = startPos if startPos else [0]*N
+		self.history = [self.currPos[:]]
 		
 	def run(self, nGen):
-		self.currPos = self.startPos
 		for gen in range(nGen):
 			self.currPos = select(branch(self.currPos, self.pointProcess), self.N)[0]
-		return self.currPos
+			self.history += [self.currPos]
 
+# INPUT array of positions and a poinProcess supporting .simulate()
+# OUTPUT array of tuples of the form (position, parent index)
 def branch(position, pointProcess):
 	n = len(position)
 	descendants = [None]*n
@@ -30,11 +33,23 @@ def branch(position, pointProcess):
 		descendants[particle] = zip([position[particle]]*nChild + children, [particle]*nChild)
 	return reduce(lambda x,y: x+y, descendants)
 
+# INPUT array of positions of descendants and the number N
+# OUTPUT tuple of the next generations and the genealogy after selection
 def select(descendants, N):
 	indices = np.argsort([d[0] for d in descendants])[-N:]
 	nextGen = [descendants[i][0] for i in indices]
 	genealogy = [descendants[i][1] for i in indices]
 	return (nextGen, genealogy)
 
-x = N_BRW(pointProcess(), 2)
-print(x.run(100))
+N = 10
+nGenerations = 100
+nBRW = N_BRW(pointProcess(), N)
+nBRW.run(nGenerations)
+x = reduce(lambda x,y:x+y, [[i]*N for i in range(nGenerations + 1)])
+y = reduce(lambda x,y:x+y, nBRW.history)
+plt.plot(x, y, 'o')
+plt.show()
+
+
+
+
